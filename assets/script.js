b249cd17722502b2abd9b444aa10f43d06c2bdd6
“Thomas <tjwhitephd@gmail.com>
2024-09-16 15:03:09 -0700

    start of tutoring session

--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,45 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
 // Collect employee data
 const collectEmployees = function() {
-  // TODO: Get user input to create and return an array of employee objects
+  // TODO: use a "while" loop to get user input to create and return an array of employee objects
+const employeesArray = []
+  const fact = true;
+while (fact) {
+  const firstName = alert("what is your first name?")
+  const lastName = alert("what is your last name?")
+  const salary = alert("what is your salary?")
+  // todo: create an object with all 3 of these values, push that object to employeesArray
+
+  var payData = {};
+    payData["01"] = firstName;
+    payData["02"] = lastName;
+    payData["03"] = salary;
+    employeesArray.push(payData); 
+
+  fact = confirm("continue (OK) or cancel");
 }
+
+  // display data alphabetically by lastName, as
+      //firstName:"John",
+      //lastName:"Smith",
+      //salary:12345;
+    // salary should default to $0 if not entered as a number (use "isNaN" function for this)
+
+
+  }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
-  // TODO: Calculate and display the average salary
+  // TODO: Calculate and display the average salary using a "template literal string"
+  // show computed/aggregate data
+console.log(`Average employee salary is $${salary}.`);
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  //"Math" object can help here... Math.random
+  console.log(`The winner is ${Math.random} employee.`);
 }
 
 /*
